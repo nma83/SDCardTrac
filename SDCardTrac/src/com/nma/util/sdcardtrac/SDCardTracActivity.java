b5a802ebb3b3456938cbbc482b3e582f312a4325
naren <narendra.acharya@gmail.com>
2012-04-01 22:57:06 +0530

    Brought up GUI with basic configuration functions. Persistence across Activity life-cycle still not handled well.

--- SDCardTrac/src/com/nma/util/sdcardtrac/SDCardTracActivity.java
+++ SDCardTrac/src/com/nma/util/sdcardtrac/SDCardTracActivity.java
@@ -9,16 +9,26 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
-public class SDCardTracActivity extends Activity {
-	private static final int START_ALARM_OFFSET = 10;
+public class SDCardTracActivity extends Activity implements OnClickListener {
+	private static final int DEFAULT_START_OFFSET_SEC = 10;
+	private static final long DEFAULT_UPDATE_INTERVAL_SEC = AlarmManager.INTERVAL_HALF_DAY;
 	
 	// Handle to running service
 	private FileObserverService.TrackingBinder serviceBind;
 	boolean boundToService = false;
+	boolean alarmSetupDone = false;
+	PendingIntent alarmIntent;
 	// Handle to database
 	private DatabaseManager trackingDB;
 	
@@ -40,23 +50,30 @@ public class SDCardTracActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        // Setup the alarm
-        setupEventCollection();
+
+        // Setup alarm intent - one time
+        Intent triggerIntent = new Intent(this, DeltaCompute.class);
+        alarmIntent = PendingIntent.getBroadcast(this, 0, triggerIntent, 0);
+        if (!alarmSetupDone) manageAlarm(true);
         // Setup the database
         trackingDB = new DatabaseManager(this);
+        
+        // Setup UI event listeners
+        ((Button)findViewById(R.id.apply_config_button)).setOnClickListener(this);
+        ((Button)findViewById(R.id.default_config_button)).setOnClickListener(this);
+        ((Button)findViewById(R.id.background_control_toggle)).setOnClickListener(this);
+        ((Button)findViewById(R.id.trigger_collect_button)).setOnClickListener(this);
+        ((Button)findViewById(R.id.show_graph_button)).setOnClickListener(this);
+        ((Button)findViewById(R.id.exit_app_button)).setOnClickListener(this);
     }
     
     @Override
     protected void onStart() {
     	super.onStart();
-    	// Start the service
-    	Intent serviceIntent = new Intent(this, FileObserverService.class);
-    	serviceIntent.setAction(Intent.ACTION_MAIN);
-    	startService(serviceIntent);
-    	// Bind to service
-    	bindService(serviceIntent, serviceConn, Context.BIND_AUTO_CREATE);
-    	// Open the database to read
-    	trackingDB.openToRead();
+    	// Start monitoring service
+    	backgroundService(true);
+        // Set default state of toggle
+        ((ToggleButton)findViewById(R.id.background_control_toggle)).setChecked(alarmSetupDone);
     }
     
     @Override
@@ -68,18 +85,101 @@ public class SDCardTracActivity extends Activity {
     	}
     }
     
+    // UI interaction, button click handler
+    @Override
+    public void onClick(View v) {
+    	int currId = v.getId();
+    	String startOffsetStr, triggerIntervalStr;
+    	boolean checked;
+    	
+    	switch (currId) {
+    	case R.id.apply_config_button:
+    		startOffsetStr = ((TextView)findViewById(R.id.init_offset_value)).getText().toString();
+    		triggerIntervalStr = ((TextView)findViewById(R.id.update_interval_value)).getText().toString();
+    		checked = ((ToggleButton)findViewById(R.id.background_control_toggle)).isChecked();
+    		updateConfig(Integer.parseInt(startOffsetStr, 10), Long.parseLong(triggerIntervalStr), checked);
+    		break;
+    	case R.id.default_config_button:
+    		((TextView)findViewById(R.id.init_offset_value)).setText(DEFAULT_START_OFFSET_SEC);
+    		((TextView)findViewById(R.id.update_interval_value)).setText((int)DEFAULT_UPDATE_INTERVAL_SEC);
+    		break;
+    	case R.id.background_control_toggle:
+    		startOffsetStr = ((TextView)findViewById(R.id.init_offset_value)).getText().toString();
+    		triggerIntervalStr = ((TextView)findViewById(R.id.update_interval_value)).getText().toString();
+    		checked = ((ToggleButton)findViewById(R.id.background_control_toggle)).isChecked();
+    		backgroundService(checked);
+    		manageAlarm(checked);
+    		updateConfig(Integer.parseInt(startOffsetStr, 10), Long.parseLong(triggerIntervalStr), checked);
+    		// Disable trigger button if background disabled
+    		((Button)findViewById(R.id.trigger_collect_button)).setEnabled(checked);
+    		break;
+    	case R.id.trigger_collect_button:
+    		Intent triggerCollect = new Intent(this, FileObserverService.class);
+    		triggerCollect.setAction(Intent.ACTION_VIEW);
+    		startService(triggerCollect);
+    		break;
+    	case R.id.exit_app_button:
+    		finish();
+    		break;
+    	}
+    }
+    
     // Helper methods
+    // Start the background service
+    private void backgroundService(boolean enable) {
+    	// Control the service
+    	Intent serviceIntent = new Intent(this, FileObserverService.class);
+    	serviceIntent.setAction(Intent.ACTION_MAIN);
+    	if (enable) {
+    		startService(serviceIntent);
+    		// Bind to service
+    		bindService(serviceIntent, serviceConn, Context.BIND_AUTO_CREATE);
+    	} else {
+    		stopService(serviceIntent);
+    	}
+    }
+
+    // Update persistent configuration
+    private void updateConfig(int startOffset, long updateInterval, boolean alarmEnabled) {
+    	SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+    	SharedPreferences.Editor prefEdit = prefs.edit();
+    	prefEdit.putInt("AlarmStartOffset", startOffset);
+    	prefEdit.putLong("AlarmTriggerInterval", updateInterval);
+    	prefEdit.putBoolean("AlarmEnabled", alarmEnabled);
+    	prefEdit.apply();
+    }
+    
     // Setup recurring collection of events
-    public void setupEventCollection() {
+    private void manageAlarm(boolean enable) {
+    	SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+    	if (enable && prefs.getBoolean("AlarmEnabled", true)) {
+    		if (alarmSetupDone) {
+    			((AlarmManager)getSystemService(Context.ALARM_SERVICE)).cancel(alarmIntent);
+    			alarmSetupDone = false;
+    		}
+            // Get configuration
+            int startOffset = prefs.getInt("AlarmStartOffset", DEFAULT_START_OFFSET_SEC);
+            long triggerInterval = prefs.getLong("AlarmTriggerInterval", DEFAULT_UPDATE_INTERVAL_SEC);
+            ((EditText)findViewById(R.id.init_offset_value)).setText(Integer.toString(startOffset));
+            ((EditText)findViewById(R.id.update_interval_value)).setText(Long.toString(triggerInterval));
+            
+            // Setup the alarm
+            setupEventCollection(startOffset, triggerInterval * 1000);
+    	} else if (!alarmSetupDone) {
+			((AlarmManager)getSystemService(Context.ALARM_SERVICE)).cancel(alarmIntent);
+			Log.d(getClass().getName(), "Cancelling alarms");
+    	}
+    }
+    
+    public void setupEventCollection(int startOffset, long triggerInterval) {
     	Log.d(this.getClass().getName(), "Setting up alarm for collection");
     	AlarmManager alarmEr = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
-    	Intent triggerIntent = new Intent(this, DeltaCompute.class);
-    	PendingIntent pendIntent = PendingIntent.getBroadcast(this, 0, triggerIntent, 0);
     	Calendar time = Calendar.getInstance();
     	time.setTimeInMillis(System.currentTimeMillis());
-    	time.add(Calendar.SECOND, START_ALARM_OFFSET);
+    	time.add(Calendar.SECOND, startOffset);
     	alarmEr.setInexactRepeating(AlarmManager.RTC_WAKEUP, time.getTimeInMillis(), 
-    			(1000 * 30), pendIntent); //AlarmManager.INTERVAL_FIFTEEN_MINUTES
+    			triggerInterval, alarmIntent); //AlarmManager.INTERVAL_FIFTEEN_MINUTES
     	Log.d(this.getClass().getName(), "Done with alarm setup");
+    	alarmSetupDone = true;
     }
 }
