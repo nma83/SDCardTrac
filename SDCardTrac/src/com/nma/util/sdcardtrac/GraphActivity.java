f64eeb23041cfa0b1be282a93603f0cddddbd065
naren <narendra.acharya@gmail.com>
2012-04-03 21:21:53 +0530

    Graph activity now plots basic tracking graph.

--- SDCardTrac/src/com/nma/util/sdcardtrac/GraphActivity.java
+++ SDCardTrac/src/com/nma/util/sdcardtrac/GraphActivity.java
@@ -11,15 +11,22 @@ import com.jjoe64.graphview.LineGraphView;
 
 import android.app.Activity;
 import android.content.ContentValues;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.widget.LinearLayout;
 
 public class GraphActivity extends Activity {
 	DatabaseManager trackingDB;
+	long startTime, endTime;
+	long maxStorage;
+	boolean lowerThanMax;
 	
 	@Override
 	public void onCreate(Bundle savedInstance) {
+		int locColor;
+		
 		super.onCreate(savedInstance);
 		setContentView(R.layout.graph);
 		
@@ -29,20 +36,65 @@ public class GraphActivity extends Activity {
 		// Plot it
 		GraphView storageGraph = new LineGraphView(this, "Storage History") {
 			@Override  
-			protected String formatLabel(double value, boolean isValueX) {  
-				if (isValueX) {  
-					// convert unix time to human time  
-					return DateFormat.getDateInstance().format(new Date((long) value*1000));  
-				} else return super.formatLabel(value, isValueX); // let the y-value be normal-formatted  
+			protected String formatLabel(double value, boolean isValueX) {
+				String retValue;
+				
+				if (isValueX) { // Format time in human readable form
+					retValue = DateFormat.getDateInstance().format(new Date((long)value));
+					Log.d(getClass().getName(), "Date is : " + retValue);
+				} else { // Format size in human readable form
+					long scaling;
+					String suffix;
+					if ((value / 1000) < 1) { // Under a KB, keep as is
+						scaling = 1;
+						suffix = "B";
+					} else if ((value / 1000000) < 1) { // KB
+						scaling = 1000;
+						suffix = "KB";
+					} else if ((value / 1000000000) < 1) { // MB
+						scaling = 1000000;
+						suffix = "MB";
+					} else { // GB
+						scaling = 1000000000;
+						suffix = "GB";
+					}
+					retValue = Integer.toString((int)(value / scaling)) + suffix;
+				}
+				//return super.formatLabel(value, isValueX); // let the y-value be normal-formatted
+				return retValue;
 			}
 		};
+
+		if (lowerThanMax) {
+			locColor = Color.rgb(100, 100, 0); // Current usage is lower than 70%
+		} else {
+			locColor = Color.rgb(200, 0, 0);
+		}
+		// Add marker for maximum storage
+		GraphViewSeries maxStorageMark = new GraphViewSeries("Maximum storage", locColor,
+				new GraphViewData[] {
+					new GraphViewData(startTime, maxStorage),
+					new GraphViewData(endTime, maxStorage)
+		});
+		
+		// Add marker for 0
+		GraphViewSeries minStorageMark = new GraphViewSeries("Maximum storage", Color.rgb(0, 0, 0),
+				new GraphViewData[] {
+					new GraphViewData(startTime, 0),
+					new GraphViewData(endTime, 0)
+		});
+
 		
 		storageGraph.addSeries(storageGraphData);
+		storageGraph.addSeries(maxStorageMark);
+		storageGraph.addSeries(minStorageMark);
 		((LinearLayout)findViewById(R.id.graph_layout)).addView(storageGraph);
 	}
 	
 	// Helpers
 	private GraphViewSeries getData() {
+		int maxUsage = 0;
+		
 		trackingDB.openToRead();
 		// TODO: use start/end time hooks
 		ArrayList <ContentValues> dbData = (ArrayList<ContentValues>)trackingDB.getValues(0, 0);
@@ -53,16 +105,29 @@ public class GraphActivity extends Activity {
 		GraphViewData [] graphData = new GraphViewData[dbData.size()];
 		int i = 0;
 		for (ContentValues d : dbData) {
-			String timeStamp = (String)d.get(DatabaseManager.ID_COLUMN);
-			String usage = (String)d.get(DatabaseManager.DELTA_COLUMN);
+			long timeStamp = Long.parseLong((String)d.get(DatabaseManager.ID_COLUMN));
+			int usage = Integer.parseInt((String)d.get(DatabaseManager.DELTA_COLUMN));
 			String changeLog = (String)d.get(DatabaseManager.LOG_COLUMN); // TODO:
-			graphData[i] = new GraphViewData(Long.parseLong(timeStamp), Integer.parseInt(usage));
-			Log.d(getClass().getName(), "Added to graph : " + Long.parseLong(timeStamp)
-					+ " - " + Integer.parseInt(usage));
+			if (i == 0) startTime = timeStamp;
+			else if (i == (dbData.size() - 1)) endTime = timeStamp;
+			
+			graphData[i++] = new GraphViewData(timeStamp, usage);
+			if (usage > maxUsage) maxUsage = usage;
+			
+//			Log.d(getClass().getName(), "Added to graph : " + Long.parseLong(timeStamp)
+//					+ " - " + Integer.parseInt(usage));
 		}
 		
 		retSeries = new GraphViewSeries(graphData);
 		trackingDB.close();
+		maxStorage = Environment.getExternalStorageDirectory().getTotalSpace();
+		if ((maxUsage / maxStorage) < 0.7) {
+			maxStorage = maxUsage;
+			lowerThanMax = true;
+		} else {
+			lowerThanMax = false;
+		}
+		
 		return retSeries;
 	}
 }
