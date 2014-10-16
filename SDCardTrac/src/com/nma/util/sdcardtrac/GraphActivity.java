41b2ad6aa4322d2e32a8b862fc0a90a6fb5cdd60
NMA <narendra_m_a@yahoo.com>
2014-10-16 20:28:14 +0530

    License inserted at the right places

--- SDCardTrac/src/com/nma/util/sdcardtrac/GraphActivity.java
+++ SDCardTrac/src/com/nma/util/sdcardtrac/GraphActivity.java
@@ -1,40 +1,38 @@
 /*
- * SDCardTrac application - keeps track of the /sdcard usage
- * Copyright (C) 2012 Narendra M.A.
+ *  StorageTrac application - keeps track of the external storage usage
+ *  Copyright (C) 2012 Narendra M.A.
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 package com.nma.util.sdcardtrac;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.text.method.LinkMovementMethod;
 import android.util.Log;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.AdapterView;
-import android.widget.LinearLayout;
-import android.widget.PopupWindow;
 import android.widget.Spinner;
-import android.widget.TextView;
-
-import java.util.Set;
 
 public class GraphActivity extends ActionBarActivity
     implements GraphFragment.OnFragmentInteractionListener {
